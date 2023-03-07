5b59f5334e6f2f06a9d4a05bd5f4d5e6f2c3a599
Leventure <102945300+LeventureQys@users.noreply.github.com>
2023-03-07 13:49:59 +0800

    update
    1

---
+++ ILBD_WebApiInterface/UserInfo.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+using namespace std;
+
+public class LoginUserInfo
+{
+public:
+    string UserID;
+    string UserName;
+    string Gender;
+    string GradeID;
+    string GradeName;
+    string GlobalGrade;
+    string GroupID;
+    string GroupName;
+    string SubjectIDs;
+    string SubjectNames;
+    int UserType;
+    int UserClass;
+    string PhotoPath;
+    string PreLoginTime;
+    string PreLoginIP;
+    string ShortName;
+    string Sign;
+    string SchoolID;
+    string SchoolName;
+    string UpdateTime;
+    string Token;
+    string LoginInfo;
+}
